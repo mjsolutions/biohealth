5c41f46a13b0ba47a5ff603b234cae346a8f0565
martinalanis <martinalanis.dev@gmail.com>
2016-08-15 13:04:32 -0500

    Inicio 15-08-16

--- app/Models/enterprise.php
+++ app/Models/enterprise.php
@@ -1,10 +1,25 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class enterprise extends Model
+class Enterprise extends Model
 {
-    //
+    // MASS ASSIGNMENT -------------------------------------------------------
+    // define which attributes are mass assignable (for security)
+    // we only want these 3 attributes able to be filled
+    protected $fillable = array('name_enterprise', 'rfc');
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    // each enterprise HAS MANY branches
+    public function branch() {
+        return $this->hasMany('App\Models\Branch'); // this matches the Eloquent model
+    }
+
+    public function department(){
+        return $this->hasManyThrough('App\Models\Branch', 'App\Models\Department');
+    }
+
+    
 }
