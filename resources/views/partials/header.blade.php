9faae6bc61d8b87d5d8f2f8fe64934f6a6cea4e1
Martin <martinalanis.dev@gmail.com>
2017-02-09 18:37:45 -0600

    correcion de ruta logout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,6 @@
 		<span>{{Auth::user()->name_employee}} : {{Auth::user()->department->name_department}}</span>
 	</div>
 	<div class="col-sm-1 col-md-1 col-lg-1 mt12">
-		<span><a id="salirLink" href="/logout">Salir</a></span>
+		<span><a id="salirLink" href="{{url('/logout')}}">Salir</a></span>
 	</div>
 </div>
